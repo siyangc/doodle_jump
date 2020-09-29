4eacd3a906a9232a7640449e23c9a735b8914f55
siyangc <csy602994381@gmail.com>
2020-09-29 14:21:58 +1000

    add canvas

---
+++ src/components/Canvas.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Canvas() {
+    return (
+        <div style={canvasStyle}>
+            
+        </div>
+    )
+}
+
+const canvasStyle = {
+    width: "300px",
+    height: "600px",
+    backgroundColor: "green"
+}
---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Canvas from './Canvas'
+export default class Game extends Component {
+    render() {
+        return (
+            <div>
+                <Canvas />
+            </div>
+        )
+    }
+}
